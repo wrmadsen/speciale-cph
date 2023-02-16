90f14547b14db0a3d0585da8ffe8eaa6d2076415
William Rohde Madsen <wrohdemadsen@icloud.com>
2023-02-17 00:57:58 +0100

    load twitter files, and radio_raw

--- R/01_load_raw_data.R
+++ R/01_load_raw_data.R
@@ -1,9 +1,22 @@
 # Load raw data
 
+# Radio data -----
+radio_raw <- read_csv("data-raw/Radio/articles_radiondekeluka.csv")
+
 # Twitter data ----
 # By keywords
-twitter_keywords_raw <- list.files(pattern = "twitter_keyword", recursive = TRUE) %>%
-  map_df(~read_csv(.))
+read_bind_twitter <- function(files){
+  
+  id <- gsub("data\\-raw\\/Twitter\\/twitter_keyword\\_|\\.csv|_.+", "", files)
+  
+  read_csv(files) %>%
+    clean_names() %>%
+    mutate(id = id)
+  
+}
+
+twitter_keywords_raw <- list.files(pattern = "twitter_keyword_.+\\.csv", recursive = TRUE) %>%
+  map_df(read_bind_twitter)
 
 # By user
 
