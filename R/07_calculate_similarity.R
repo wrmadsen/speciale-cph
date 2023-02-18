78165636192063be6de06041912666ffe586c6b1
William Rohde Madsen <wrohdemadsen@icloud.com>
2023-02-18 17:23:05 +0100

    trying to fix 'where is x_document' error

---
+++ R/07_calculate_similarity.R
@@ -0,0 +1,132 @@
+# Calculate similarity
+
+# Cosine similarity -----
+
+# Subset each in weighted dfm
+radio_for_sim <- dfm_subset(master_dfm_tf_idf, group == "Radio")
+nrow(radio_for_sim)
+
+twitter_for_sim <- dfm_subset(master_dfm_tf_idf, group == "Twitter")
+nrow(twitter_for_sim)
+
+# Calculate similarity
+cosine_sim_scores <- textstat_simil(x = twitter_for_sim, 
+                                    y = radio_for_sim,
+                                    method = "cosine")
+
+# To tibble
+cosine_sim <- cosine_sim_scores %>%
+  as.matrix() %>%
+  as.data.frame() %>%
+  rownames_to_column(var = "twitter_document") # named after whatever variable refers to obs length
+
+# Check
+#cosine_sim %>% filter(twitter_document == "text9556")  # where is the radio_document?
+
+# Pivot
+cosine_sim_long <- cosine_sim %>%
+  mutate(cosine_id = row_number()) %>%
+  select(cosine_id, twitter_document, everything()) %>%
+  pivot_longer(cols = c(3:ncol(.)), names_to = "radio_document", values_to = "cosine_sim") %>%
+  mutate(cosine_id = row_number()) %>%
+  pivot_longer(cols = c(radio_document, twitter_document), names_to = "text_name", values_to = "document_no")
+
+cosine_sim_long <- cosine_sim_long %>%
+  mutate(document_factor = as.factor(document_no))
+
+# Check
+cosine_sim_long %>%
+  group_by(text_name, document_no) %>%
+  summarise(n = n()) %>%
+  distinct(n)
+
+cosine_sim_long %>% distinct(document_no) # check all documents are in, both Twitter and radio
+9555 + 4511
+
+# Add dates and group names
+# Join by document number twice
+# For Twitter, then Radio
+dates_and_groups <- master_dt %>%
+  transmute(document_no = document, date, document_factor = as.factor(document_no)) %>%
+  tibble()
+
+dates_and_groups %>% distinct(document_no) # check all documents are in, both Twitter and radio
+
+dates_and_groups %>% arrange(desc(date)) # arrange by date
+
+dates_and_groups %>% group_by(document_no) %>% summarise(n = n()) %>% distinct(n) # number of dates per text_no
+
+dates_and_groups %>% filter(document_no == "text65") # check random doc
+
+dates_and_groups %>% filter(is.na(document_no))
+
+cosine_sim_long %>% distinct(text_name)
+
+cosine_sim_long %>% filter(is.na(cosine_sim))
+
+cosine_sim_long %>% group_by(text_name) %>% filter(date == max(date))
+
+
+
+# Join function
+cosine_sim_full <- right_join(cosine_sim_long, dates_and_groups, by = "document_no")
+
+
+# Radio should show 2017 to 2023
+# text9556 is the oldest for radio ndeke
+# text14065 is the newest for radio ndeke
+# Twitter should be 2014 to 2023
+# Are they switched?
+cosine_sim_full %>%
+  filter(text_name == "twitter_document") %>% #distinct(date) %>% view
+  .$date %>% summary()
+
+cosine_sim_long %>% group_by(text_name) %>% distinct(text_name)
+
+cosine_sim_full %>% filter(document_no == "text9600") %>% distinct(text_name) # where is the radio_document?
+
+cosine_sim_full %>% filter(document_no == "text4111") %>% distinct(text_name) # where is the twitter_document?
+
+
+# Why 
+cosine_sim_full %>%
+  #distinct(text_name, document_no) %>%
+  group_by(document_no, text_name) %>%
+  summarise(n = n())
+
+
+# Filter those similarity pairs whose dates are within range
+# Calculate difference in days
+# Then choose the date of the radio document
+# as the future reference marker
+cosine_sim_wide <- cosine_sim_full %>%
+  select(cosine_sim, cosine_id, text_name, date) %>%
+  # group_by(cosine_id) %>%
+  # mutate(days_diff = difftime(date, lag(date), units = c("days")),
+  #        days_diff = abs(days_diff) %>% as.integer(),
+  #        days_diff = max(days_diff, na.rm = TRUE))
+  pivot_wider(names_from = text_name, values_from = date) %>%
+  mutate(days_diff = difftime(radio_document, twitter_document, units = c("days")),
+         days_diff = abs(days_diff) %>% as.integer())
+
+# These two date summaries should be similar
+# Not sure why that was not the case in beginning
+# Something happens in the above code with pivot_wider
+summary(cosine_sim_wide$twitter_document)
+summary(cosine_sim_wide$radio_document)
+
+cosine_sim_sub <- cosine_sim_wide %>%
+  filter(days_diff < 10)
+
+# Return
+cosine_sim_final <- cosine_sim_sub %>%
+  select(date = radio_document, cosine_sim)
+
+cosine_sim_final
+
+summary(cosine_sim_final$date)
+
+
+
+
+
