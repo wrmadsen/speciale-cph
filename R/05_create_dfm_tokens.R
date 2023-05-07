c5b60dff27f51fd374922b401f29ec19e55ef81a
William Rohde Madsen <wrohdemadsen@icloud.com>
2023-05-07 23:11:21 +0200

    dfm_trime by min termfreq and max docfreq

--- R/05_create_dfm_tokens.R
+++ R/05_create_dfm_tokens.R
@@ -110,6 +110,7 @@ groups <- c("coalition des mouvements de lazawad",
             "yerewolo debout sur les remparts",
             "lac debo tv",
             "joliba fm",
+            "ndjoni sango",
             #"wagner",
             "ouverture media",
             "actuel media",
@@ -194,9 +195,6 @@ create_tokens <- function(dt){
   
   # Remove stopwords before stemming the tokens
   tokens <- tokens_remove(tokens, pattern = french_stopwords)
-  
-  # Stem tokens
-  tokens <- tokens_wordstem(tokens, language = "fr")
   
   tokens
   
@@ -314,13 +312,18 @@ master_text <-master_text %>%
   mutate(spike_no = if_else(is.na(spike_no), 0, spike_no), 
          spike_binary = if_else(spike_no > 0, 1, 0) %>% as.factor)
 
-master_text <- bind_rows(master_text %>%
-                           filter(orient != "Pro-Russia") %>%
-                           mutate(sub_group = "Non-Russian total"),
-                         master_text %>%
-                           filter(orient == "Pro-Russia") %>%
-                           mutate(sub_group = "Pro-Russian total")) %>%
-  bind_rows(master_text)
+# Add observations that are pro-Russia or Non-Russian total
+# Remove this?
+# This doubles the number of rows, making the model process slower
+# master_text <- bind_rows(master_text %>%
+#                            filter(orient != "Pro-Russia") %>%
+#                            mutate(sub_group = "Non-Russian total"),
+#                          master_text %>%
+#                            filter(orient == "Pro-Russia") %>%
+#                            mutate(sub_group = "Pro-Russian total")) %>%
+#   bind_rows(master_text)
+
+
 
 # Run functions ----
 master_dt <- master_text %>%
@@ -331,32 +334,63 @@ master_dt <- master_text %>%
   recode_strings_before_tokenizing(strings_to_recode, .) %>%
   convert_to_dt()
 
-# Subset
-# must_include <- c("russie", "poutine", "wagner", "russes", "russia")
-# 
-# master_dt %>%
-#   filter()
+# Remove certain patterns from text
+patterns_to_remove <- c("var player = new mediaelementplayerplayer")
+
+patterns_to_remove <- patterns_to_remove %>%
+  paste0(., collapse = "|")
+
+master_dt <- master_dt %>%
+  mutate(text = gsub(patterns_to_remove, "", text))
 
 master_tokens <- master_dt %>%
   create_tokens() %>%
-  replace_tokens(., tokens_to_recode)
+  # At least two character length
+  tokens_select(., min_nchar = 2)
+
+# Stem tokens
+master_tokens_stemmed <- master_tokens %>%
+  tokens_wordstem(., language = "fr")
+
+# Replace tokens
+master_tokens_stemmed <- master_tokens_stemmed %>%
+  replace_tokens(., tokens_to_recode) #%>%
+# Remove certain tokens
+#tokens_remove(., total_to_be_removed)
 
 master_dfm <- master_tokens %>%
-  create_dfm()
+  create_dfm() %>%
+  # Remove tokens by count
+  # Must have minimum term count
+  # But maximum document count
+  dfm_trim(.,
+           min_termfreq = 50,
+           termfreq_type = "count",
+           max_docfreq = 1000,
+           docfreq_type = "count")
 
 master_dfm_tf_idf <- master_dfm %>%
   dfm_tfidf()
 
 master_tokens_tbl <- master_dfm %>%
-  convert_dfm_to_tibble(., master_dt) %>%
-  filter(nchar(token) > 1)
-
+  convert_dfm_to_tibble(., master_dt)
 
 # Print or view ----
 
-topfeatures(master_dfm, n = 10)
+## View most popular tokens and phrases ----
+popular_tokens <- topfeatures(master_dfm, n = 1000) %>%
+  data.frame() %>%
+  rownames_to_column("token") %>%
+  tibble() %>%
+  rename("count" = ".")
 
-topfeatures(master_dfm_tf_idf, n = 10)
+reference_tokens <- tibble(non_stemmed = master_tokens %>% as.character(),
+                           stemmed = master_tokens_stemmed %>% as.character()) %>%
+  distinct()
+
+popular_tokens <- left_join(popular_tokens, reference_tokens, by = c("token" = "stemmed"))
+
+popular_tokens
 
 master_tokens_tbl %>%
   group_by(token) %>%
@@ -364,7 +398,7 @@ master_tokens_tbl %>%
   arrange(-n) #%>% view("count")
 
 # Check individual tokens with view()
-master_tokens_tbl %>% filter(token == "poutin") %>%
+master_tokens_tbl %>% filter(token == "ndjon") %>%
   arrange(text_nchar) #%>% view("token")
 
 
