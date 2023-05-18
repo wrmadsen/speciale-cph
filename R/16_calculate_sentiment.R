a580f6e34e0cceda8c76c71113f7acddb557da79
William Rohde Madsen <wrohdemadsen@icloud.com>
2023-05-18 19:50:07 +0200

    afinn adjusting, trade off b.t. original docs and mean topic prop

--- R/16_calculate_sentiment.R
+++ R/16_calculate_sentiment.R
@@ -2,11 +2,21 @@
 
 # Join tokens with sentiment dictionary -----
 master_tokens_sentiment <- left_join(master_tokens_tbl, afinn, by = c("token" = "token")) %>%
-  select(document, orient, group, sub_group, text_nchar, text, token, date, week, year, month, afinn_mean, afinn_median)
+  select(document, orient, group, sub_group, text_nchar, text, token, date, week, year, month,
+         afinn_mean, afinn_median)
 
 # Deal with outliers, bias?
 master_tokens_sentiment <- master_tokens_sentiment %>%
-  mutate(across(c(afinn_mean, afinn_median), ~if_else(. > 2 | . < -2, NA_integer_, .)))
+  mutate(across(c(afinn_mean, afinn_median), as.numeric)) #%>%
+#mutate(across(c(afinn_mean, afinn_median), ~if_else(. > 1 | . < -1, NA_integer_, .))) %>%
+# mutate(across(c(afinn_mean, afinn_median),
+#               ~case_when(. > 1 ~ as.numeric(1),
+#                          . < -1 ~ as.numeric(-1),
+#                          . < 1 & . > 0 ~ as.numeric(1),
+#                          . > -1 & . < 0 ~ as.numeric(-1),
+#                          is.na(.) ~ as.numeric(NA),
+#                          .default = as.numeric(.))
+#               ))
 
 # test <- master_tokens_sentiment %>%
 #   filter(text_nchar < 400) %>%
@@ -45,6 +55,9 @@ sentiment_per_doc <- master_tokens_sentiment %>%
   ungroup() %>%
   select(document, afinn_document, n_tokens, text, text_nchar)
 
+# Average sentiment
+summary(sentiment_per_doc$afinn_document)
+
 # See some
 sentiment_per_doc %>%
   filter(is.na(n_tokens))
@@ -54,8 +67,7 @@ sentiment_per_doc %>%
 
 # Add thetas (topics) ----
 thetas_that_will_be_joined <- master_dt_thetas_long %>%
-  select(sub_group, document, date, month, year, topic_no, topic_name, topic_proportion, text_nchar, text, url) %>%
-  filter(year(date) >= 2020)
+  select(sub_group, document, date, month, year, topic_no, topic_name, topic_proportion, text_nchar, text, url)
 
 sentiment_per_doc_thetas <- left_join(thetas_that_will_be_joined,
                                       sentiment_per_doc %>% select(-c(text, text_nchar)),
@@ -63,17 +75,84 @@ sentiment_per_doc_thetas <- left_join(thetas_that_will_be_joined,
   relocate(text_nchar, text, url, .after = last_col())
 
 # Subset data ----
-sentiment_per_doc_thetas %>% filter(is.na(n_tokens))
 
-# Remove NAs
-# And select topics for document
+## Remove NAs ----
+sentiment_per_doc_thetas <- sentiment_per_doc_thetas %>%
+  filter(!is.na(n_tokens) & !is.na(afinn_document)) 
+
+## Select by topic proportion ----
+
+### Determine best way ----
+# Trade-off b.t.
+# Number of unique documents
+# High topic proportion (mean)
+filter_by_topic_proportion <- function(input, rate){
+  
+  input %>%
+    filter(topic_proportion >= rate) %>%
+    transmute(rate = rate, document, sub_group, topic_proportion)
+  
+}
+
+best_topic_selection <- bind_rows(filter_by_topic_proportion(sentiment_per_doc_thetas, 0.0),
+                                  filter_by_topic_proportion(sentiment_per_doc_thetas, 0.1),
+                                  filter_by_topic_proportion(sentiment_per_doc_thetas, 0.2),
+                                  filter_by_topic_proportion(sentiment_per_doc_thetas, 0.3),
+                                  filter_by_topic_proportion(sentiment_per_doc_thetas, 0.4),
+                                  filter_by_topic_proportion(sentiment_per_doc_thetas, 0.5),
+                                  filter_by_topic_proportion(sentiment_per_doc_thetas, 0.6),
+                                  filter_by_topic_proportion(sentiment_per_doc_thetas, 0.7))
+
+best_topic_selection
+
+data_for_plot <- best_topic_selection %>%
+  group_by(sub_group, rate) %>%
+  summarise(docs = n_distinct(document),
+            mean = mean(topic_proportion)*100) %>%
+  group_by(sub_group) %>%
+  mutate(docs_original = max(docs),
+         docs_share = docs/docs_original*100)
+
+data_for_plot %>%
+  ggplot(.,
+         aes(x = mean,
+             y = docs_share)) +
+  geom_vline(xintercept = 50) +
+  geom_line(aes(colour = sub_group, linetype = sub_group), size = 2,
+            show.legend = FALSE) +
+  geom_point(size = 3) +
+  geom_text_repel(data = data_for_plot %>% filter(sub_group == "Ndjoni Sango" & rate == 0.3),
+                  aes(label = paste0(rate*100, "% minimum")),
+                  family = theme_font,
+                  size = 4.5) +
+  scale_color_manual(name = "", values = colours_groups) +
+  scale_linetype_manual(name = "", values = lines_group) +
+  scale_x_continuous(breaks = seq(0, 100, 25), limits = c(0, 100)) +
+  facet_wrap(~sub_group, scales = "free_y") +
+  labs(title = "Balance between a high topic proportion and many documents",
+       y = "Share of original documents, %",
+       x =  "Mean topic proportion, %",
+       caption = "Source: William Rohde Madsen.") +
+  theme_speciale
+
+#save_plot_speciale("output-figures/appendix_select_topic_prop.png")
+
+### Actually do it -----
+# slice_max() is also an option
 sentiment_per_doc_thetas_sub <- sentiment_per_doc_thetas %>%
-  filter(!is.na(n_tokens) & !is.na(afinn_document)) %>%
-  group_by(document) %>%
-  slice_max(order_by = topic_proportion, n = 10) %>%
-  ungroup()
+  filter(topic_proportion >= 0.2)
 
-# Summary after slice_max -----
+
+# Summary post -----
+
+# Variance
+sentiment_per_doc_thetas_sub$topic_proportion %>% summary()
+
+# Average sentiment
+sentiment_per_doc_thetas_sub %>%
+  group_by(sub_group, topic_name) %>%
+  summarise(mean = mean(afinn_document)) %>%
+  filter(grepl("FACA", topic_name))
 
 # Summary
 summary(sentiment_per_doc_thetas_sub$topic_proportion)
